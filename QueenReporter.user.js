66c5edc81c6b11ff362b8d2a1a09ac473e76882c
geisterfurz007 <geisterfurz007@gmail.com>
2019-02-22 22:39:40 +0100

    Some CSS tweaking

--- QueenReporter.user.js
+++ QueenReporter.user.js
@@ -372,7 +372,7 @@ function constructFlagOption(smolText, bigText, value) {
 	const radioButton = $("<input>").addClass('s-radio').addClass('m0').attr("type", "radio").attr("id", radioId).attr('name', 'comment-flag-type').attr('value', value);
 	
 	radioButton.click(() => {
-		$(".js-modal-submit.disabled-button").prop("disabled", false).removeClass("disabled-button");
+		$(".js-modal-submit.disabled-button").prop("disabled", false).removeClass("disabled-button").css("cursor", "pointer");
 	});
 
 	inputDiv.append(radioButton);
